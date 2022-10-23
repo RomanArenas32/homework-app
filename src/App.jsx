73ea37d5712a10610d3c8e026fb4bb76e74b71b2
Roman Arenas <roman.ea3232@gmail.com>
2022-10-23 19:54:37 -0300

    CardAppReact

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './index.css';
+import QuoteBox from './assets/component/QuoteBox';
+
+function App() {
+  
+  
+ 
+  return (
+   <div className='container' >
+    <QuoteBox 
+    />
+   </div>
+  )
+}
+
+export default App
